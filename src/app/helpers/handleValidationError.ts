cb3c4a1e03c5ca9cec55d5143bc32538d7c78f0b
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-08-18 00:34:59 +0600

    make all things/components for a basic server and make user and auth related all file and make api for these

---
+++ src/app/helpers/handleValidationError.ts
@@ -0,0 +1,22 @@
+import type mongoose from "mongoose";
+import type { TErrorSources, TGenericErrorResponse } from "../interfaces/error.types";
+
+export const handleValidationError = (
+  err: mongoose.Error.ValidationError
+): TGenericErrorResponse => {
+  const errorSources: TErrorSources[] = [];
+
+  const errors = Object.values(err.errors);
+  errors.forEach((errorObject: any) =>
+    errorSources.push({
+      path: errorObject.path,
+      message: errorObject.message,
+    })
+  );
+
+  return {
+    statusCode: 400,
+    message: "Validation Error",
+    errorSources,
+  };
+};
