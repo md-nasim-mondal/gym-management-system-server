223915180e798f8bb2198ea64c70b163d76fefc4
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-08-18 01:19:09 +0600

    update server initial get message

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
-    message: "Welcome to Tour Management System Backend",
+    message: "Welcome to GYM Management System Server",
   });
 });
 
